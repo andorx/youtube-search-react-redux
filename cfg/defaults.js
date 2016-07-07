014f9a7165dd767ea3c03e7cc499d0c2ed1dd9ec
Duc N. PHAM <pngduc@gmail.com>
2016-07-07 15:01:14 +0700

    Removed redundant loader

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -29,20 +29,8 @@ function getDefaultModules() {
         loader: 'style-loader!css-loader'
       },
       {
-        test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      },
-      {
         test: /\.scss/,
         loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-      },
-      {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
-      {
-        test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
       },
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
