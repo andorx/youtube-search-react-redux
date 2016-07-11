1ef497f581455be2ecd6272457a10b7b7df43a3e
Duc N. PHAM <pngduc@gmail.com>
2016-07-11 18:02:03 +0700

    Defined actions for UI interactions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,81 @@
-/* Combine all available reducers to a single root reducer.
- *
- * CAUTION: When using the generators, this file is modified in some places.
- *          This is done via AST traversal - Some of your formatting may be lost
- *          in the process - no functionality should be broken though.
- *          This modifications only run once when the generator is invoked - if
- *          you edit them, they are not updated again.
- */
 import { combineReducers } from 'redux';
-/* Populated by react-webpack-redux:reducer */
-const reducers = {};
-module.exports = combineReducers(reducers);
+
+import * as actionTypes from '../constants/actionTypes';
+
+const reducers = {
+  search
+};
+
+function search(state = {
+  isProcessing: false,
+  keyword: '',
+  filters: {
+    order: '',
+    videoDuration: '',
+    videoType: ''
+  },
+  pageToken: '',
+  nextPageToken: '',
+  prevPageToken: '',
+  results: []
+}, action) {
+  switch(action.type) {
+    case actionTypes.CHANGE_KEYWORD: {
+      return Object.assign({}, state, {
+        keyword: action.keyword
+      });
+    }
+
+    case actionTypes.SEARCH_FOR_VIDEOS: {
+      return Object.assign({}, state, {
+        isProcessing: true
+      });
+    }
+
+    case actionTypes.RECEIVE_SEARCH_RESULTS: {
+      return Object.assign({}, state, {
+        isProcessing: false,
+        results: action.results
+      });
+    }
+
+    case actionTypes.NAVIGATE_TO_NEXT_PAGE: {
+      return Object.assign({}, state, {
+        pageToken: action.pageToken
+      });
+    }
+
+    case actionTypes.NAVIGATE_TO_PREV_PAGE: {
+      return Object.assign({}, state, {
+        pageToken: action.pageToken
+      });
+    }
+
+    case actionTypes.SET_CONTENT_FILTER: {
+      var filters = Object.assign({}, state.filters, {
+        [action.filterType]: action.filterValue
+      });
+
+      return Object.assign({}, state, {
+        filters
+      });
+    }
+
+    case actionTypes.SET_PREV_PAGE_TOKEN: {
+      return Object.assign({}, state, {
+        prevPageToken: action.prevPageToken
+      });
+    }
+
+    case actionTypes.SET_NEXT_PAGE_TOKEN: {
+      return Object.assign({}, state, {
+        nextPageToken: action.nextPageToken
+      });
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default combineReducers(reducers);
