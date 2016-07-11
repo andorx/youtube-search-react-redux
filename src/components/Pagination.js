d46153ba8c0af3b8b504e0206fd6af07cf407e3b
Duc N. PHAM <pngduc@gmail.com>
2016-07-11 18:09:33 +0700

    Modify pagination component to navigate to prev/next results page

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,44 +1,39 @@
 import React from 'react';
 
 class Pagination extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+  }
+
+  handleNavigate(direction) {
+    if (direction === 'prev') {
+      if (!this.props.prevPageToken) return;
+      this.props.actions.navigateToPrevPage(this.props.prevPageToken);
+    } else if (direction === 'next') {
+      if (!this.props.nextPageToken) return;
+      this.props.actions.navigateToNextPage(this.props.nextPageToken);
+    }
+
+    this.props.actions.fetchSearchResults();
   }
 
   render() {
     return (
       <ul className="pagination">
         <li className="page-item">
-          <a href="#" className="disabled">
+          <a href='javascript: void(0)'
+            className={this.props.prevPageToken ? '' : 'disabled' }
+            onClick={this.handleNavigate.bind(this, 'prev')}>
+            &larr;
             Previous
           </a>
         </li>
-        <li className="page-item active">
-          <a href="#">
-            1
-          </a>
-        </li>
         <li className="page-item">
-          <a href="#">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a href="#">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <span>...</span>
-        </li>
-        <li className="page-item">
-          <a href="#">
-            12
-          </a>
-        </li>
-        <li className="page-item">
-          <a href="#">
+          <a href='javascript: void(0)'
+            className={this.props.nextPageToken ? '' : 'disabled' }
+            onClick={this.handleNavigate.bind(this, 'next')}>
             Next
+            &rarr;
           </a>
         </li>
       </ul>
