b4ce26777d27fd72a4e9f6e5edc5c33e74e6a9a9
Duc N. PHAM <pngduc@gmail.com>
2016-07-11 18:08:58 +0700

    Created KeywordInput component

---
+++ src/components/KeywordInput.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class KeywordInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const keyword = this.refs.keyword.value;
+
+    if (event.which === 13) {
+      event.preventDefault();
+
+      this.props.actions.changeKeyword(keyword);
+      this.props.actions.fetchSearchResults();
+    }
+  }
+
+  render() {
+    return (
+      <div className="form-group search-box">
+        <input
+          type="text"
+          className="form-input input-lg"
+          placeholder="Type anything to start searching..."
+          disabled={this.props.isFetching == true}
+          ref="keyword"
+          onKeyDown={this.handleSubmit}
+          />
+      </div>
+    );
+  }
+}
+
+export default KeywordInput;
