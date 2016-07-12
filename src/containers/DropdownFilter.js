2c90abf2d161102a36799872894a3d4b9361132f
Duc N. PHAM <pngduc@gmail.com>
2016-07-12 17:29:33 +0700

    Removed old comments

--- src/containers/DropdownFilter.js
+++ src/containers/DropdownFilter.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { setContentFilter, resetPageToken, fetchSearchResults } from '../actions/actions';
 import Dropdown from '../components/Dropdown';
 
-// Note: this statement attachs `dispatch` method to props,
-// that will be used later in event handler follow dispatch(fetchData())
 function mapStateToProps(state) {
   const props = {
     isProcessing: state.search.isProcessing
@@ -13,8 +11,6 @@ function mapStateToProps(state) {
   return props;
 }
 
-// Note: this state attachs specific actions to props,
-// that will be used later follow this.props.actions.fetchData()
 function mapDispatchToProps(dispatch) {
   return  {
     onFilterChange: function(filterType, filterValue) {
