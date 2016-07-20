72fdaf6c27765e7eeef96a016c5cdb4daec533be
Duc N. PHAM <pngduc@gmail.com>
2016-07-20 17:00:35 +0700

    Updated state of SearchResults container

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -5,6 +5,7 @@ import VideoList from '../components/VideoList';
 function mapStateToProps(state) {
   const props = {
     results: state.search.results,
+    videoDetails: state.search.videoDetails,
     isProcessing: state.search.isProcessing
   };
 
