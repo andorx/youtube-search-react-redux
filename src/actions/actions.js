685486adf5368ada0ca05676e9fa640d0ca22823
Duc N. PHAM <pngduc@gmail.com>
2016-08-08 10:47:00 +0700

    Added comment for actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,6 +3,11 @@ import fetch from 'isomorphic-fetch';
 import * as actionTypes from '../constants/actionTypes';
 import * as YoutubeAPI from '../constants/YoutubeAPI';
 
+/**
+ * Changing keyword action
+ * @param  {string} keyword Search keyword
+ * @return {Object}         Minimal action description
+ */
 export function changeKeyword(keyword) {
   return {
     type: actionTypes.CHANGE_KEYWORD,
@@ -10,12 +15,21 @@ export function changeKeyword(keyword) {
   };
 }
 
+/**
+ * Searching action
+ * @return {Object} Minimal action description
+ */
 export function searchForVideos() {
   return {
     type: actionTypes.SEARCH_FOR_VIDEOS
   };
 }
 
+/**
+ * Receiving search results action
+ * @param  {Array} results Search results
+ * @return {Object}         Minimal action description
+ */
 export function receiveSearchResult(results) {
   return {
     type: actionTypes.RECEIVE_SEARCH_RESULTS,
@@ -23,6 +37,11 @@ export function receiveSearchResult(results) {
   };
 }
 
+/**
+ * Searching failure action
+ * @param  {String} reason Reason
+ * @return {Object}        Minimal action description
+ */
 export function searchVideoFailure(reason) {
   return {
     type: actionTypes.SEARCH_FOR_VIDEOS_FAILURE,
@@ -30,12 +49,21 @@ export function searchVideoFailure(reason) {
   }
 }
 
+/**
+ * Fetching video details action
+ * @return {Object} Minimal action description
+ */
 export function requestVideoDetails() {
   return {
     type: actionTypes.REQUEST_VIDEO_DETAILS
   }
 }
 
+/**
+ * Receiving video details action
+ * @param  {Object} results Video details
+ * @return {Object}         Minimal action description
+ */
 export function receiveVideoDetails(results) {
   return {
     type: actionTypes.RECEIVE_VIDEO_DETAILS,
@@ -43,6 +71,11 @@ export function receiveVideoDetails(results) {
   }
 }
 
+/**
+ * Requesting video failure action
+ * @param  {String} reason Reason
+ * @return {Object}        Minimal action description
+ */
 export function requestVideoDetailsFailure(reason) {
   return {
     type: actionTypes.REQUEST_VIDEO_DETAILS_FAILURE,
@@ -50,6 +83,12 @@ export function requestVideoDetailsFailure(reason) {
   }
 }
 
+/**
+ * Changing content filter action
+ * @param {String} filterType  Filter type
+ * @param {String} filterValue Filter value
+ * @return {Object}            Minimal action description
+ */
 export function setContentFilter(filterType, filterValue) {
   return {
     type: actionTypes.SET_CONTENT_FILTER,
@@ -58,18 +97,31 @@ export function setContentFilter(filterType, filterValue) {
   };
 }
 
+/**
+ * Navigating to next page action
+ * @return {Object} Minimal action description
+ */
 export function navigateToNextPage() {
   return {
     type: actionTypes.NAVIGATE_TO_NEXT_PAGE
   };
 }
 
+/**
+ * Navigating to prev page action
+ * @return {Object} Minimal action description
+ */
 export function navigateToPrevPage() {
   return {
     type: actionTypes.NAVIGATE_TO_PREV_PAGE
   };
 }
 
+/**
+ * Setting previous page token
+ * @param {String} prevPageToken Previous page token
+ * @return {Object}              Minimal action description
+ */
 export function setPrevPageToken(prevPageToken) {
   return {
     type: actionTypes.SET_PREV_PAGE_TOKEN,
@@ -77,6 +129,11 @@ export function setPrevPageToken(prevPageToken) {
   }
 }
 
+/**
+ * Setting next page token
+ * @param {String} nextPageToken Next page token
+ * @return {Object}              Minimal action description
+ */
 export function setNextPageToken(nextPageToken) {
   return {
     type: actionTypes.SET_NEXT_PAGE_TOKEN,
@@ -84,12 +141,20 @@ export function setNextPageToken(nextPageToken) {
   }
 }
 
+/**
+ * Resetting page token
+ * @return {Object} Minimal action description
+ */
 export function resetPageToken() {
   return {
     type: actionTypes.RESET_PAGE_TOKEN
   };
 }
 
+/**
+ * Fetching search results async action
+ * @return {Function} Tail function that will be called by dispatch
+ */
 export function fetchSearchResults() {
   return (dispatch, getState) => {
     const state = getState();
@@ -99,13 +164,16 @@ export function fetchSearchResults() {
       pageToken
     } = state.search;
 
+    // Building search endpoint
     const SEARCH_API = YoutubeAPI.URL + 'search' +
       searchQueryBuilder(YoutubeAPI.KEY, keyword, filters, pageToken);
 
+    // Dispatch searching action
     dispatch(searchForVideos());
 
     return fetch(SEARCH_API)
       .then(response => {
+        // Request error handler
         if (response.status >= 400) {
           return response.json()
             .then(function(json) {
@@ -116,20 +184,24 @@ export function fetchSearchResults() {
         return response.json();
       })
       .then(searchResults => {
+        // Dispatch receiving search results
         dispatch(receiveSearchResult(searchResults.items));
 
+        // Dispatch setting prev page token
         if (searchResults.prevPageToken) {
           dispatch(setPrevPageToken(searchResults.prevPageToken));
         } else {
           dispatch(setPrevPageToken(''));
         }
 
+        // Dispatch setting next page token
         if (searchResults.nextPageToken) {
           dispatch(setNextPageToken(searchResults.nextPageToken));
         } else {
           dispatch(setNextPageToken(''));
         }
 
+        // Dispatch fetching video details if there are items in search results
         if (searchResults.items && searchResults.items.length > 0) {
           dispatch(fetchVideoDetails(searchResults.items));
         }
@@ -137,25 +209,35 @@ export function fetchSearchResults() {
         return searchResults.items;
       })
       .catch(reason => {
+        // Dispatch searching failure
         dispatch(searchVideoFailure(reason));
       });
   };
 }
 
+/**
+ * Fetching video videos async action
+ * @param  {Array} videoSnippets Search video results
+ * @return {Object}              Tail function that will be called by dispatch
+ */
 export function fetchVideoDetails(videoSnippets) {
   return dispatch => {
     let VIDEO_DETAILS_API,
+        // Filter out video ids
         videoIds = videoSnippets.map(function(snippet) {
           return snippet.id.videoId;
         });
 
+    // Building fetch video details endpoint
     VIDEO_DETAILS_API = YoutubeAPI.URL + 'videos' +
       videoDetailsQueryBuilder(YoutubeAPI.KEY, videoIds);
 
+    // Dispatch requesting video details
     dispatch(requestVideoDetails(videoIds));
 
     return fetch(VIDEO_DETAILS_API)
       .then(response => {
+        // Request error handler
         if (response.status >= 400) {
           return response.json()
             .then(function(json) {
@@ -168,22 +250,33 @@ export function fetchVideoDetails(videoSnippets) {
       .then(results => {
         var videoDetails = {};
 
+        // Build up a object contains video details
         if (results.items && results.items.length > 0) {
           results.items.forEach(function(item) {
             videoDetails[item.id] = item;
           });
 
+          // Dispatch receving video details
           dispatch(receiveVideoDetails(videoDetails));
         }
 
         return videoDetails;
       })
       .catch(reason => {
+        // Dispatch fetching failure
         dispatch(requestVideoDetailsFailure(reason));
       });
   };
 }
 
+/**
+ * Building search video query
+ * @param  {String} key       YoutubeAPI key
+ * @param  {String} keyword   Search keyword
+ * @param  {Object} filters   Content filters
+ * @param  {String} pageToken Page token
+ * @return {String}           Search video query
+ */
 function searchQueryBuilder(key, keyword, filters, pageToken) {
   var params = {
       key,
@@ -198,6 +291,12 @@ function searchQueryBuilder(key, keyword, filters, pageToken) {
   return paramsBuilder(params);
 }
 
+/**
+ * Building fetch video details query
+ * @param  {String} key      YoutubeAPI key
+ * @param  {String} videoIds Comma-delimited video ids list
+ * @return {String}          Fetch video details query
+ */
 function videoDetailsQueryBuilder(key, videoIds) {
   var params = {
       key,
@@ -209,6 +308,11 @@ function videoDetailsQueryBuilder(key, videoIds) {
   return paramsBuilder(params);
 }
 
+/**
+ * Building query string from given parameters
+ * @param  {Object} params Parameters
+ * @return {String}
+ */
 export function paramsBuilder(params) {
   var query = '?' + Object.keys(params).map(function(key) {
     if (params[key]) {
@@ -219,6 +323,6 @@ export function paramsBuilder(params) {
   }).join('&');
 
   return query
-    .replace(/&+/gm, '&')
-    .replace(/&+$/gm,'');
+    .replace(/&+/gm, '&') // Reduce sequence ampersands
+    .replace(/&+$/gm,''); // Remove ending ampersands
 }
